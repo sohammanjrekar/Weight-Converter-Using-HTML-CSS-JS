f15fdee3f74199ceb97310e93862031b9a291308
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-04-08 17:18:24 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,24 +1,11 @@
-const inputEl = document.getElementById("input");
-const errorEl = document.getElementById("error");
-const resultEl = document.getElementById("result");
-let errorTime;
-let resultTime;
-function updateResults() {
-  if (inputEl.value <= 0 || isNaN(inputEl.value)) {
-    errorEl.innerText = "Please enter a valid number!";
-    clearTimeout(errorTime);
-    errorTime = setTimeout(() => {
-      errorEl.innerText = "";
-      inputEl.value = "";
-    }, 2000);
-  } else {
-    resultEl.innerText = (+inputEl.value / 2.2).toFixed(2);
-    clearTimeout(resultTime);
-    resultTime = setTimeout(() => {
-      resultEl.innerText = "";
-      inputEl.value = "";
-    }, 10000);
-  }
-}
+document.getElementById('output').style.visibility = 'hidden'
 
-inputEl.addEventListener("input", updateResults);
+document.getElementById('lbsinput').addEventListener('input', function (e) {
+  document.getElementById('output').style.visibility = 'visible'
+  let lbs = e.target.value
+  document.getElementById('gramsoutput').innerHTML = lbs * 453.592
+
+  document.getElementById('kgoutput').innerHTML = lbs / 2.2046
+  
+  document.getElementById('ozeoutput').innerHTML = lbs * 16
+})
